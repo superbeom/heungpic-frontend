772e1c86321ac6e631fcfa2d698f0c6ae0988997
superbeom <it.super.beom@gmail.com>
2019-08-26 13:38:40 +0900

    #4.3 Apollo Client

---
+++ src/Apollo/LocalState.js
@@ -0,0 +1,23 @@
+export const defaults = {
+  isLoggedIn: Boolean(localStorage.getItem("token")) || false
+};
+
+export const resolvers = {
+  Mutation: {
+    logUserIn: (_, { token }, { cache }) => {
+      localStorage.setItem("token", token);
+      cache.writeData({
+        data: {
+          isLoggedIn: true
+        }
+      });
+      window.location.reload();
+      return null;
+    },
+    logUserOut: () => {
+      localStorage.removeItem("token");
+      window.location = "/";
+      return null;
+    }
+  }
+};
